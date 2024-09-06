c81645db908c5a53f278dc85ff640fa3f8216c7f
Keegan-Ohlman-Student <144161693+Keegan-Ohlman-Student@users.noreply.github.com>
2024-09-06 18:45:39 +0000

    Added most of the color change button js

---
+++ Eventful Interactions/Scripts/app.js
@@ -0,0 +1,21 @@
+let hideButton = document.getElementById("btn-hide");
+let showButton = document.getElementById("btn-show");
+let colorButton = document.getElementById("btn-change-color");
+
+let paragraph2 = document.getElementById("p2");
+let paragraph3 = document.getElementById("p3");
+
+let color = document.getElementById("txt-color").value;
+
+hideButton.addEventListener('click', () => {
+    paragraph2.style.display = 'none';
+})
+
+showButton.addEventListener('click', () => {
+    paragraph2.style.display = 'block';
+})
+
+colorButton.addEventListener('click', () => {
+    paragraph3.style.backgroundColor = color;
+})
+
