28813dd3b4effb8c7f107b7c4ebdedc85b93489d
Keegan-Ohlman-Student <144161693+Keegan-Ohlman-Student@users.noreply.github.com>
2024-09-06 16:32:10 +0000

    Finished adding the functionality of the buttons for Getting Friendly with the DOM

--- GettingFriendlyWithTheDOM/Scripts/app.js
+++ GettingFriendlyWithTheDOM/Scripts/app.js
@@ -1,6 +1,11 @@
 let hideButton = document.getElementById("btn-hide");
 let showButton = document.getElementById("btn-show");
+let paragraph2 = document.getElementById("p2");
 
 hideButton.addEventListener('click', () => {
-    alert();
+    paragraph2.style.display = 'none';
+})
+
+showButton.addEventListener('click', () => {
+    paragraph2.style.display = 'block';
 })
