6a482ce009ff14c64d96eb9614baaacc76a1ed15
Keegan-Ohlman-Student <144161693+Keegan-Ohlman-Student@users.noreply.github.com>
2024-09-12 18:26:12 +0000

    Added the functionality to the shape changing for Arrow Functions and More Interactivity

--- ArrowFunctionsAndMoreInteractivity/Scripts/app.js
+++ ArrowFunctionsAndMoreInteractivity/Scripts/app.js
@@ -23,10 +23,39 @@ colorButton.addEventListener('click', () => {
 })
 
 shape.addEventListener('change', () => {
-    let shapeToChange = document.getElementByID("shape");
+    let shapeToChange = document.getElementById("shape");
+
+    if(shape.value === "Square")
+    {
+        shapeToChange.style.width = "100px";
+        shapeToChange.style.height = "100px";
+        shapeToChange.style.borderRadius = "0px";
+        shapeToChange.style.borderLeft = "solid black";
+        shapeToChange.style.borderRight = "solid black";
+        shapeToChange.style.borderTop = "solid black";
+        shapeToChange.style.borderBottom = "solid black";
+        
+    }
 
     if(shape.value === "Circle")
     {
+        shapeToChange.style.width = "100px";
+        shapeToChange.style.height = "100px";
         shapeToChange.style.borderRadius = "100px";
+        shapeToChange.style.borderLeft = "solid black";
+        shapeToChange.style.borderRight = "solid black";
+        shapeToChange.style.borderTop = "solid black";
+        shapeToChange.style.borderBottom = "solid black";
+    }
+
+    if(shape.value === "Triangle")
+    {
+        shapeToChange.style.width =  0;
+        shapeToChange.style.height = 0;
+        shapeToChange.style.borderLeft = "75px solid transparent";
+        shapeToChange.style.borderRight = "75px solid transparent";
+        shapeToChange.style.borderBottom = "100px solid black";
+        shapeToChange.style.borderTop = "solid transparent";
+        shapeToChange.style.borderRadius = "0px";
     }
 })
