966993d30ffd182d1e4a93db0e615dd4933725bb
Keegan-Ohlman-Student <144161693+Keegan-Ohlman-Student@users.noreply.github.com>
2024-09-19 18:20:12 +0000

    Finsihed adding the Form Handling and Validation scripts to make it work properly

--- FormHandlingAndValidation/Scripts/app.js
+++ FormHandlingAndValidation/Scripts/app.js
@@ -3,18 +3,23 @@ let button = document.getElementById('submitButton');
 button.addEventListener('click', (e) => {
     let username = document.getElementById('username').value;
     let email = document.getElementById('email').value;
+    let age = document.getElementById('age').value;
 
-    let regex = /@./i;
-
-    if (username.length < 3)
+    if(username.length < 3)
     {
         e.preventDefault();
         document.getElementById('usernameError').innerHTML = "Your username must be at least 3 characters long.";
     }
 
-    if(!email.match(regex))
+    if(!email.includes(".") || !email.includes("@"))
     {
         e.preventDefault();
-        document.getElementById('emailError').innerHTML = "Your email must include a '@' and a '.' symbol";
+        document.getElementById('emailError').innerHTML = "Your email must include a '@' and a '.' symbol.";
+    }
+
+    if(age <= 10 || age >= 100)
+    {
+        e.preventDefault();
+        document.getElementById('ageError').innerHTML = "Your age must be between 10 and 100.";
     }
 })
