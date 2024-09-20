cd02c620730cdde4c794002676e339d27aa66334
Keegan-Ohlman-Student <144161693+Keegan-Ohlman-Student@users.noreply.github.com>
2024-09-20 17:31:24 +0000

    Made the delete button work, but it deletes all list items in the Interactive List task

--- InteractiveList/Scripts/app.js
+++ InteractiveList/Scripts/app.js
@@ -4,6 +4,18 @@ let list = document.getElementById('list');
 button.addEventListener('click', () => {
     let itemToAdd = document.getElementById('itemToAdd').value;
     let item = document.createElement("li");
+    let deleteButton = document.createElement("button");
+
     item.appendChild(document.createTextNode(itemToAdd));
     list.appendChild(item);
+    deleteButton.appendChild(document.createTextNode("Delete"));
+    item.appendChild(deleteButton);
+
+    list.addEventListener('click', (e) => {
+        if(e.target.tagName === 'BUTTON')
+        {
+            item.remove();
+        }
+    })
 })
+
